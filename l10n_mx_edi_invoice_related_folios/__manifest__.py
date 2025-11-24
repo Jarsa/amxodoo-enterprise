d0ab58a6dbb1cb5c17d94d40b6d1944d2aac9a93
Hector Meraz Chavez <hector.meraz@jarsa.com>
2025-11-24 13:34:41 -0600

    [ADD] l10n_mx_edi_invoice_related_folios: add related UUID on invoice

---
+++ l10n_mx_edi_invoice_related_folios/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
+
+{
+    "name": "Add Related Folios to invoices",
+    "version": "17.0.1.0.0",
+    "category": "Report",
+    "author": "Jarsa",
+    "website": "https://www.jarsa.com",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "l10n_mx_edi",
+    ],
+    "data": [
+        "reports/report_invoice.xml",
+    ],
+    "installable": True,
+}
