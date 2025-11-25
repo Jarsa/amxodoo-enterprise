672188ef0f12298e1f9b402dbe73a2b2e4af4eb9
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2025-11-24 20:48:52 -0600

    [FIX] l10n_mx_edi_decimal_fix: use float_round to round HALF-UP

---
+++ l10n_mx_edi_decimal_fix/models/l10n_mx_edi_document.py
@@ -0,0 +1,12 @@
+from odoo import api, models
+from odoo.tools.float_utils import float_round
+
+
+class L10nMxEdiDocument(models.Model):
+    _inherit = "l10n_mx_edi.document"
+
+    @api.model
+    def _add_base_cfdi_values(self, cfdi_values):
+        res = super()._add_base_cfdi_values()
+        res["float_round"] = float_round
+        return res
