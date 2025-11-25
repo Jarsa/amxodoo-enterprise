3e0f74fb67bfbdaf0d30cf65b02ef44ebde390bb
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2025-11-24 19:09:39 -0600

    [FIX] l10n_mx_edi_decimal_fix: get MontoTotalPagos with 2 decimals

--- l10n_mx_edi_decimal_fix/__manifest__.py
+++ l10n_mx_edi_decimal_fix/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "MX EDI Decimal Fix",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "summary": "Fix EDI Documents when decimal precision of currency is greater than 2",
     "author": "Jarsa",
     "website": "https://www.jarsa.com",
