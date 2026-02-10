b39e2cdea49198e1fba21a7ed1bb3158b6e5fe93
Hector Meraz Chavez <hector.meraz@jarsa.com>
2026-02-10 16:46:49 -0600

    [FIX] l10n_mx_edi_decimal_fix:  prevent negative ImpSaldoInsoluto in payment CFDI task#30923

--- l10n_mx_edi_decimal_fix/__manifest__.py
+++ l10n_mx_edi_decimal_fix/__manifest__.py
@@ -1,9 +1,9 @@
 {
     "name": "MX EDI Decimal Fix",
-    "version": "17.0.1.0.4",
+    "version": "17.0.1.0.5",
     "summary": "Fix EDI Documents when decimal precision of currency is greater than 2",
     "author": "Jarsa",
-    "website": "https://www.jarsa.com",
+    "website": "https://github.com/amxodoo/enterprise",
     "license": "LGPL-3",
     "category": "Localization/Mexico",
     "depends": [
