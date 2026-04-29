7b157c52b307670dd91acebeaf155ff12f52d61a
Hector Meraz Chavez <hector.meraz@jarsa.com>
2026-04-29 15:15:14 -0600

    [FIX] l10n_mx_edi_decimal_fix: Fix rounding to six decimals task#31180

--- l10n_mx_edi_decimal_fix/__manifest__.py
+++ l10n_mx_edi_decimal_fix/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "MX EDI Decimal Fix",
-    "version": "17.0.1.0.7",
+    "version": "17.0.1.0.8",
     "summary": "Fix EDI Documents when decimal precision of currency is greater than 2",
     "author": "Jarsa",
     "website": "https://github.com/amxodoo/enterprise",
