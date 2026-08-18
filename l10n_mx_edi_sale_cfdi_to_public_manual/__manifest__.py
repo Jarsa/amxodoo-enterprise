cf171cf1d0ff767e71f6fc658873e150d1ef34fb
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2026-08-18 14:44:10 -0600

    [ADD] l10n_mx_edi_sale_cfdi_to_public_manual: keep CFDI to public unchecked on sale orders task#32278

---
+++ l10n_mx_edi_sale_cfdi_to_public_manual/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "MX EDI Sale CFDI to Public Manual",
+    "version": "17.0.1.0.0",
+    "summary": "Keep the 'CFDI to Public' checkbox of sale orders unchecked "
+    "unless the user sets it",
+    "author": "Jarsa, Odoo Mexican Association (AMOdoo)",
+    "website": "https://github.com/amxodoo/enterprise",
+    "license": "LGPL-3",
+    "category": "Localization/Mexico",
+    "depends": [
+        "l10n_mx_edi_sale",
+    ],
+}
